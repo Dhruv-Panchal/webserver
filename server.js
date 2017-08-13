c7740210e7ef96b2353167fcbcb1fe3f637ba89b
Dhruv Panchal <dhruvpanchal96@gmail.com>
2017-08-13 07:32:27 +0000

    update language

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/about', function(req, res) {
-    res.send('About us');
+    res.send('About us!');
 });
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.PORT || PORT, function() {
